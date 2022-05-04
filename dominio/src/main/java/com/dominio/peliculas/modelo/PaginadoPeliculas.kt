ba418d98cb7080c8ccbb3ae332479fbdd1d0ce93
cristian.martinez <davick16@gmail.com>
2022-05-04 11:50:34 -0500

    validacion creacion de pelicula

--- dominio/src/main/java/com/dominio/peliculas/modelo/PaginadoPeliculas.kt
+++ dominio/src/main/java/com/dominio/peliculas/modelo/PaginadoPeliculas.kt
@@ -1,9 +1,18 @@
 package com.dominio.peliculas.modelo
 
+import com.dominio.peliculas.excepcion.ExcepcionNulo
+
 data class PaginadoPeliculas(
     val page: Int?,
     val results: ArrayList<Pelicula>?,
     val total_pages: Int?,
     val total_results: Int?,
 ) {
+
+    init {
+        if (validacionNulo()) throw ExcepcionNulo()
+    }
+
+    private fun validacionNulo(): Boolean = page == null || results == null || total_pages == null || total_results == null
+
 }
