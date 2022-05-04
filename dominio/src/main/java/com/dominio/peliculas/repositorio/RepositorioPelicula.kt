98ac2d9e3e44a62edb7c02e16a1d21431175547f
cristian.martinez <davick16@gmail.com>
2022-05-04 08:54:35 -0500

    creacion repositorio pelicula

--- dominio/src/main/java/com/dominio/peliculas/repositorio/RepositorioPelicula.kt
+++ dominio/src/main/java/com/dominio/peliculas/repositorio/RepositorioPelicula.kt
@@ -1,4 +1,12 @@
 package com.dominio.peliculas.repositorio
 
-class RepositorioPelicula {
+import com.dominio.peliculas.modelo.Pelicula
+
+interface RepositorioPelicula {
+
+    fun peliculaPorId(id: Int): Pelicula
+
+    fun guardarListaPeliculas(listaPeliculas: List<Pelicula>)
+
+    fun listaPeliculas(): List<Pelicula>
 }
