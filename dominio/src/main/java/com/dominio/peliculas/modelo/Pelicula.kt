0df32f9fd31cfccfffa0628a206cbcfc00433362
cristian.martinez <davick16@gmail.com>
2022-05-03 21:55:13 -0500

    creacion atributos clase pelicula

--- dominio/src/main/java/com/dominio/peliculas/modelo/Pelicula.kt
+++ dominio/src/main/java/com/dominio/peliculas/modelo/Pelicula.kt
@@ -3,8 +3,14 @@ package com.dominio.peliculas.modelo
 data class Pelicula(
 
     val id: Int?,
-
-    ) {
+    val original_language: String,
+    val original_title: String,
+    val poster_path: String,
+    val vote_average: Float,
+    val release_date: String,
+    val adult: Boolean,
+    val overview: String,
+) {
 
 
 }
