741beb9ab6d3af0c93d02e75641f3bcae059366e
cristian.martinez <davick16@gmail.com>
2022-05-04 11:51:16 -0500

    validacion creacion pelicula

--- dominio/src/main/java/com/dominio/peliculas/modelo/Pelicula.kt
+++ dominio/src/main/java/com/dominio/peliculas/modelo/Pelicula.kt
@@ -1,16 +1,22 @@
 package com.dominio.peliculas.modelo
 
+import com.dominio.peliculas.excepcion.ExcepcionNulo
+
 data class Pelicula(
 
     val id: Int?,
-    val original_language: String,
-    val original_title: String,
-    val poster_path: String,
-    val vote_average: Float,
-    val release_date: String,
-    val adult: Boolean,
-    val overview: String,
+    val original_language: String?,
+    val original_title: String?,
+    val poster_path: String?,
+    val vote_average: Float?,
+    val release_date: String?,
+    val overview: String?,
 ) {
+    init {
+        if (validacionNulo()) throw ExcepcionNulo()
+    }
 
+    private fun validacionNulo(): Boolean = id == null || original_language == null || original_title == null || poster_path == null
+            || vote_average == null || release_date == null || overview == null
 
 }
