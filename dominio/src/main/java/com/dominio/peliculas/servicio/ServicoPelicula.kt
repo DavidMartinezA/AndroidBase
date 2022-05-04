19e4037c87695585b20f00e719d11ee8abcf9ab5
cristian.martinez <davick16@gmail.com>
2022-05-04 08:54:16 -0500

    creacion servicio pelicula

--- dominio/src/main/java/com/dominio/peliculas/servicio/ServicoPelicula.kt
+++ dominio/src/main/java/com/dominio/peliculas/servicio/ServicoPelicula.kt
@@ -1,4 +1,21 @@
 package com.dominio.peliculas.servicio
 
-class ServicoPelicula {
+import com.dominio.peliculas.modelo.Pelicula
+import com.dominio.peliculas.repositorio.RepositorioPelicula
+
+class ServicoPelicula(var repositorioPelicula: RepositorioPelicula) {
+
+    fun consultarListaPeliculas(): List<Pelicula> {
+        return repositorioPelicula.listaPeliculas()
+    }
+
+    fun guardarListaPeliculas(listaPelicula: List<Pelicula>) {
+        repositorioPelicula.guardarListaPeliculas(listaPelicula)
+
+    }
+
+    fun peliculaPorId(id: Int): Pelicula {
+        return repositorioPelicula.peliculaPorId(id)
+
+    }
 }
