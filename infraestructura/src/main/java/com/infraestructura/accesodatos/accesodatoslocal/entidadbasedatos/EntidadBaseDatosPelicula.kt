50037cd8d02dd493fdd63deaf3b30aefdbd77fda
cristian.martinez <davick16@gmail.com>
2022-05-04 21:17:43 -0500

    creacion de EntidadBaseDatosPelicula

---
+++ infraestructura/src/main/java/com/infraestructura/accesodatos/accesodatoslocal/entidadbasedatos/EntidadBaseDatosPelicula.kt
@@ -0,0 +1,14 @@
+package com.infraestructura.accesodatos.accesodatoslocal.entidadbasedatos
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.dominio.peliculas.modelo.Pelicula
+
+@Entity
+class EntidadBaseDatosPelicula(
+    @PrimaryKey
+    var pagina: Int?,
+    var resultadoPeliculas: ArrayList<Pelicula>?,
+    var paginasTotales: Int?,
+    var resultadoTotal: Int?,
+)
