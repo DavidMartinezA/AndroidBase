54a4008c84d5ce01b46ae35c5838baa58b84a2ab
cristian.martinez <davick16@gmail.com>
2022-05-15 16:34:29 -0500

    implementacion page object test

--- presentacion/src/androidTest/java/com/androidbase/listapeliculas/vista/ActividadListaPeliculasTest.kt
+++ presentacion/src/androidTest/java/com/androidbase/listapeliculas/vista/ActividadListaPeliculasTest.kt
@@ -1,3 +1,63 @@
 package com.androidbase.listapeliculas.vista
 
-class ActividadListaPeliculasTest
+import androidx.test.espresso.intent.Intents
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import com.androidbase.R
+import com.androidbase.actividadlistapeliculaspageobject.ActividadListaPeliculasPageObject
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ActividadListaPeliculasTest {
+
+    @Rule
+    @JvmField
+
+    var mActivityTestRule = ActivityScenarioRule(ActividadListaPeliculas::class.java)
+    private val actividadListaPeliculasPageObject = ActividadListaPeliculasPageObject()
+
+    @Before
+    fun borrarBaseDatos() {
+        InstrumentationRegistry.getInstrumentation().targetContext.deleteDatabase(R.string.nombre_base_datos.toString())
+    }
+
+    @Before
+    fun obtenerActividadDos() {
+        Intents.init()
+    }
+
+    @After
+    fun cerrarActividadDos() {
+        Intents.release()
+    }
+
+    @Test
+    fun mostrarListaPeliculas_parametrosCorrectos_ListaPeliculas() {
+
+        //Given
+        mActivityTestRule.scenario
+        actividadListaPeliculasPageObject
+            .existeTituloApp()
+            .existeScrollView()
+            .existeRecyclerView()
+            .existeCartelPelicula()
+            .existeTituloPelicula()
+            .existeInfoTextoTituloPelicula()
+            .existeTituloIdioma()
+            .existeInfoTextoIdioma()
+            .existeInfoTextoVotacion()
+            .existeTituloVotacion()
+            .existeBotonVerDetallePeliculas()
+
+        //When
+        actividadListaPeliculasPageObject
+            .clickBotonVerDetalles()
+
+        //Then
+        actividadListaPeliculasPageObject.verificarLanzamientoActividadDetalle()
+        mActivityTestRule.scenario.close()
+    }
+
+}
