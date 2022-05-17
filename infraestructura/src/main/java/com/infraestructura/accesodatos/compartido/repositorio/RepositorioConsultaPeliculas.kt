6a97594db2786e19e89386e2320e0a03337ad74b
cristian.martinez <davick16@gmail.com>
2022-05-17 14:04:07 -0500

    refactorizacion corutinas en infraestructura dos

--- infraestructura/src/main/java/com/infraestructura/accesodatos/compartido/repositorio/RepositorioConsultaPeliculas.kt
+++ infraestructura/src/main/java/com/infraestructura/accesodatos/compartido/repositorio/RepositorioConsultaPeliculas.kt
@@ -5,8 +5,6 @@ import com.dominio.peliculas.repositorio.RepositorioPelicula
 import com.infraestructura.accesodatos.accesodatosapi.servicioapi.ServicioApi
 import com.infraestructura.accesodatos.accesodatoslocal.anticorrupcion.TraductorPagina
 import com.infraestructura.accesodatos.accesodatoslocal.basedatos.BaseDatosPaginaPeliculas
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.time.LocalDateTime
 import javax.inject.Inject
 
@@ -17,23 +15,22 @@ class RepositorioConsultaPeliculas @Inject constructor(
 
     private val paginaPeliculasDao = baseDatosPaginaPeliculas.paginaPeliculasdao()
     private val traductorPagina = TraductorPagina()
-    private val dispatchers = Dispatchers.IO
 
     override suspend fun guardarPaginaPeliculas(paginaPeliculas: PaginadoPeliculas) {
         val entidadPaginaPeliculas = traductorPagina.desdeDominioABaseDatos(paginaPeliculas)
         paginaPeliculasDao.insertar(entidadPaginaPeliculas)
     }
 
-    override suspend fun obtenerPaginaPeliculas(): List<PaginadoPeliculas> = withContext(this.dispatchers) {
+    override suspend fun obtenerPaginaPeliculas(): List<PaginadoPeliculas> {
 
         val paginaPeliculas = paginaPeliculasDao.obtener()
         val diaHoy = LocalDateTime.now().dayOfWeek.value
 
         if (paginaPeliculas.isNullOrEmpty() || paginaPeliculas.last().diaRegistro != diaHoy) {
             paginaPeliculasDao.insertar(traductorPagina.desdeDominioABaseDatos(servicioApi.obtenerPagina()))
-            return@withContext listOf(servicioApi.obtenerPagina())
+            return listOf(servicioApi.obtenerPagina())
         } else {
-            return@withContext traductorPagina.desdeBaseDatosADominio(paginaPeliculas)
+            return traductorPagina.desdeBaseDatosADominio(paginaPeliculas)
         }
     }
 }
