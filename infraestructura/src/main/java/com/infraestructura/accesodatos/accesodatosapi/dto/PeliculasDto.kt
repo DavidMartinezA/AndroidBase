bce3e1a6c0b0f1f63ff4609d55027b49c1c1750c
cristian.martinez <davick16@gmail.com>
2022-05-18 21:45:49 -0500

    implementacion dto infraestructura

---
+++ infraestructura/src/main/java/com/infraestructura/accesodatos/accesodatosapi/dto/PeliculasDto.kt
@@ -0,0 +1,14 @@
+package com.infraestructura.accesodatos.accesodatosapi.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class PeliculasDto(
+
+    @SerializedName("page") private var pagina: Int?,
+    @SerializedName("results") var resultadoPeliculas: List<ListaResultadosDto>?,
+    @SerializedName("total_pages") private var paginasTotales: Int?,
+    @SerializedName("total_results") private var resultadosTotales: Int?,
+) {
+
+
+}
