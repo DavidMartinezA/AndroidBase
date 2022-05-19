c26587ba639f5c81d9c23502f5e8217c6579c5fb
cristian.martinez <davick16@gmail.com>
2022-05-18 21:46:41 -0500

    refactorizacion type converter

--- infraestructura/src/main/java/com/infraestructura/accesodatos/accesodatoslocal/basedatos/BaseDatosPaginaPeliculas.kt
+++ infraestructura/src/main/java/com/infraestructura/accesodatos/accesodatoslocal/basedatos/BaseDatosPaginaPeliculas.kt
@@ -2,15 +2,12 @@ package com.infraestructura.accesodatos.accesodatoslocal.basedatos
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.infraestructura.accesodatos.accesodatoslocal.convertidordatos.ConvertidorDatos
-import com.infraestructura.accesodatos.accesodatoslocal.dao.PaginaPeliculasDao
+import com.infraestructura.accesodatos.accesodatoslocal.dao.PeliculasDao
 import com.infraestructura.accesodatos.accesodatoslocal.entidadbasedatos.EntidadBaseDatosPelicula
 
 @Database(entities = [EntidadBaseDatosPelicula::class], version = 1, exportSchema = false)
-@TypeConverters(ConvertidorDatos::class)
 abstract class BaseDatosPaginaPeliculas : RoomDatabase() {
 
-    abstract fun paginaPeliculasdao(): PaginaPeliculasDao
+    abstract fun peliculasDao(): PeliculasDao
 
 }
